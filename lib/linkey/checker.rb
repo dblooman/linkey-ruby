db81fd2b1f0be3f40840753acd2038b4d0aaf9ac
DaveBlooman <david.blooman@gmail.com>
2014-03-24 15:50:10 +0000

    setup task

---
+++ lib/linkey/checker.rb
@@ -0,0 +1,9 @@
+require 'linkey'
+require 'open-uri'
+
+class Linkey::Checker < Linkey::CheckResponse
+  def test
+    puts "test"
+    # status
+  end
+end
--- lib/linkey/cli.rb
+++ lib/linkey/cli.rb
@@ -1,6 +1,7 @@
 require 'thor'
 require 'linkey/html'
 require 'linkey/ping'
+require 'linkey/checker'
 
 class Linkey::CLI < Thor
   include Thor::Actions
@@ -21,5 +22,11 @@ class Linkey::CLI < Thor
   def check(url, base, reg, filename)
     scan(url, filename)
     status(url, base, reg, filename)
+  end
+
+  desc "status check URL", "A linkey job using predetermined URL's"
+  def smoke
+    smoke = Linkey::Checker.new#(path)
+    smoke.test
   end
 end
